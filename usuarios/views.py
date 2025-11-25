090892cded41bb9e3fbf729bff6f18352172a2b3
EsdrasMOS <esdras.mosilva@gmail.com>
2025-11-25 12:20:13 -0300

    Alterações para funcinoamento do dashboard - 1

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,56 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Usuario
+from django.contrib.auth.forms import UserCreationForm
+from socios.models import Socio
+from funcionarios.models import Funcionario
+from .forms import UsuarioCreationForm  # Adicione esta importação
+
+def registro_socio(request):
+    if request.method == 'POST':
+        form = UsuarioCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.tipo_usuario = Usuario.IS_SOCIO
+            user.save()
+            
+            Socio.objects.create(
+                usuario=user,
+                nome=request.POST.get('nome'),
+                cpf=request.POST.get('cpf'),
+                telefone=request.POST.get('telefone', ''),
+                email=request.POST.get('email', ''),
+                data_nascimento=request.POST.get('data_nascimento') or None
+            )
+            
+            login(request, user)
+            messages.success(request, 'Cadastro realizado com sucesso!')
+            return redirect('dashboard_socio')
+    else:
+        form = UsuarioCreationForm()
+    return render(request, 'socios/registro_socio.html', {'form': form})
+
+def registro_funcionario(request):
+    if request.method == 'POST':
+        form = UsuarioCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.tipo_usuario = Usuario.IS_FUNCIONARIO
+            user.save()
+            
+            Funcionario.objects.create(
+                usuario=user,
+                nome=request.POST.get('nome'),
+                funcao=request.POST.get('funcao'),
+                identificacao=request.POST.get('identificacao')
+            )
+            
+            login(request, user)
+            messages.success(request, 'Cadastro realizado com sucesso!')
+            return redirect('dashboard_funcionario')
+    else:
+        form = UsuarioCreationForm()
+    return render(request, 'funcionarios/registro_funcionario.html', {'form': form})
 
 def pagina_inicial(request):
     return render(request, 'index.html')
@@ -34,12 +84,6 @@ def login_funcionario(request):
 def logout_view(request):
     logout(request)
     return redirect('pagina_inicial')
-
-def registro_socio(request):
-    return render(request, 'socios/registro_socio.html')
-
-def registro_funcionario(request):
-    return render(request, 'funcionarios/registro_funcionario.html')
 
 @login_required
 def dashboard_socio(request):
